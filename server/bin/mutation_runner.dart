1692d01593faae44a8a68e82ffec62c65d1a74b9
Luke Church <lukechurch@google.com>
2016-09-28 18:15:21 +0100

    First code based demo for driving reload operational
    Start the iOS simulator
    From the flit/myapp folder run
    dart ../server/bin/mutation_runner.dart
    Adjust the element list in diagnostics dart from [4] to [4,5,6] and back.
    
    Needs very considerable cleanup, including fixing race conditions

--- server/bin/mutation_runner.dart
+++ server/bin/mutation_runner.dart
@@ -1,9 +1,12 @@
 import 'dart:io' as io;
+import 'dart:async';
 import 'package:flutter_tools/src/android/android_sdk.dart';
 import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/doctor.dart';
 import 'package:flutter_tools/src/base/context.dart';
 import 'package:flutter_tools/src/base/logger.dart';
+import 'package:flutter_tools/src/hot.dart';
+
 
 // import 'package:flutter_tools/src/commands/run.dart';
 // import 'package:flutter_tools/src/commands/install.dart';
@@ -28,13 +31,38 @@ main(List<String> args) async {
 
   print ("About to create Run");
 
-  RunCommand runCmd = new RunCommand('/Users/gbracha/flutter', "/Users/gbracha/flit/myapp/lib/main.dart");
-  await runCmd.run();
-  print('ElementTree');
-  print('VM service = ' + runCmd.vmService.toString());
-  print('isolate = ' + runCmd.vmService.vm.mainView.uiIsolate.toString());
+  RunCommand runCmd = new RunCommand(
+    '/Users/lukechurch/GitRepos/flutter',
+    "/Users/lukechurch/GitRepos/flit/myapp/lib/main.dart");
+
+  runCmd.verifyThenRunCommand();
+  HotRunner hotRunner;
+
+  for (int i = 0; i < 20; i++) {
+    await new Future.delayed(new Duration(seconds: 1));
+    print ('Waiting');
+    print('ElementTree');
+    print('VM service = ' + runCmd.vmService.toString());
+    hotRunner = runCmd.hotRunner;
+
+    print('Hot runner = ${hotRunner.toString()}');
+    print('VM service = ${hotRunner.vmService}');
+  }
+
+  print ("===========================================");
+  print ("TODO: Something more sensible than this");
+  print ("done waiting app should have started by now");
+  print ("===========================================");
+
+  while (true) {
+    print ("attempting restart");
+    await hotRunner.restart(fullRestart: true);
+    print ("Restart complete");
+    await new Future.delayed(new Duration(seconds: 5));
+  }
+
+  // print('isolate = ' + runCmd.vmService.vm.mainView.uiIsolate.toString());
   //print(runCmd.vmService.vm.mainView.uiIsolate.flutterDebugReturnElementTree());
-  print('done');
 
 
 // InstallCommand installCommand = new InstallCommand();
