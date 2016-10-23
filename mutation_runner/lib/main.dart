c60df3dffcdaf307f102fa6d388fa48d977a0e97
danrubel <danrubel@google.com>
2016-10-23 13:52:43 +0100

    refactor app launch to use HotRunner

--- mutation_runner/lib/main.dart
+++ mutation_runner/lib/main.dart
@@ -1,66 +1,62 @@
 import 'dart:io' as io;
 import 'dart:async';
-import 'package:flutter_tools/src/android/android_sdk.dart';
+
+import 'package:flutter_tools/src/base/common.dart';
+import 'package:flutter_tools/src/build_info.dart';
+import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/device.dart';
 import 'package:flutter_tools/src/doctor.dart';
 import 'package:flutter_tools/src/base/context.dart';
 import 'package:flutter_tools/src/base/logger.dart';
+import 'package:flutter_tools/src/globals.dart';
 import 'package:flutter_tools/src/hot.dart';
 
+import 'package:path/path.dart' as path;
 
-// import 'package:flutter_tools/src/commands/run.dart';
-// import 'package:flutter_tools/src/commands/install.dart';
-
-import 'run_command_new.dart';
-
+import 'package:flutter_tools/src/resident_runner.dart';
 
 main(List<String> args) async {
-
-  // Initialize globals.
-  context[Logger] = new StdoutLogger();
-  context[DeviceManager] = new DeviceManager();
-  Doctor.initGlobal();
-  context[AndroidSdk] = AndroidSdk.locateAndroidSdk();
-
-  String flutterPath = '../../GitRepos/flutter';
-  String appMain = "lib/myapp.dart";
+  String flutterRoot = '../../flutter';
+  String target = "lib/myapp.dart";
+  String route = "/";
   String diagPath = "lib/diagnostics.dart";
 
-  print ("About to create Run");
+  // Initialize globals.
+  Cache.flutterRoot = path.normalize(path.absolute(flutterRoot));
+  context[Logger] = new StdoutLogger();
+  context[DeviceManager] = new DeviceManager();
+//  context[AndroidSdk] = AndroidSdk.locateAndroidSdk();
+  Doctor.initGlobal();
 
-  RunCommand runCmd = new RunCommand(flutterPath, appMain);
+  List<Device> allDevices = await deviceManager.getAllConnectedDevices();
 
-  print('created RunCommand');
-  runCmd.verifyThenRunCommand();
+  DebuggingOptions debugOptions = new DebuggingOptions.enabled(
+    BuildMode.debug,
+    startPaused: false,
+    observatoryPort: kDefaultObservatoryPort,
+  );
+  HotRunner hotRunner = new HotRunner(
+    allDevices.first,
+    target: target,
+    debuggingOptions: debugOptions,
+  );
 
-  print('verified RunCommand');
+  Completer<DebugConnectionInfo> debugInfoCompleter =
+      new Completer<DebugConnectionInfo>();
+  hotRunner.run(
+    connectionInfoCompleter: debugInfoCompleter,
+    route: route,
+    shouldBuild: true,
+  );
+  DebugConnectionInfo debugInfo = await debugInfoCompleter.future;
 
-  HotRunner hotRunner;
+  await hotRunner.connectToServiceProtocol(debugInfo.port);
 
-  for (int i = 0; i < 20; i++) {
-    await new Future.delayed(new Duration(seconds: 1));
-    print ('Waiting');
-    print('ElementTree');
-    hotRunner = runCmd.hotRunner;
-
-    print('Hot runner = ${hotRunner.toString()}');
-    print('VM service = ${hotRunner.vmService}');
-  }
-
-
-  print ("===========================================");
-  print ("TODO: Something more sensible than this");
-  print ("done waiting app should have started by now");
-  print ("===========================================");
-
-  var m = hotRunner.currentView.uiIsolate.flutterDebugReturnElementTree();
-  print(await m);
-
-  new io.File(diagPath)
-    .watch().listen((_) async {
-      print ("attempting restart");
-      await hotRunner.restart(fullRestart: true);
-      print ("Restart complete");
-      await new Future.delayed(new Duration(seconds: 5));
-    });
+  new io.File(diagPath).watch().listen((io.FileSystemEvent event) async {
+    print(">>> ${event.type}, ${event.path}");
+    print("attempting restart");
+    await hotRunner.restart(fullRestart: true);
+    print("Restart complete");
+    await new Future.delayed(new Duration(seconds: 5));
+  });
 }
