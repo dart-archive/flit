0da1cecf096814bb2d42af44620388b134f68501
Luke Church <lukechurch@google.com>
2016-11-02 16:26:01 +0000

    Add licences

--- mutation_runner/lib/main.dart
+++ mutation_runner/lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2016 the Dart project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
 import 'dart:io' as io;
 import 'dart:async';
 import 'dart:convert';
